aec2ef91d06f26d7bcd9ae1d2dccfe917951986b
Naomi King <naomi.k@goodnotesapp.com>
2024-10-22 16:54:44 +0100

    Implemented Kv database

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
 import {ticket} from './ticket.js';
 
-
+// Deno database setup - just for testing, will be replaced with enviroment variable or AWS platform database :)
+const currentBreached = await Deno.openKv();
 
 // Handle request function to handle hard coded ticket for now
 function handleRequest(ticketID, ticketName, ticketURL, eventType) {
-
     switch(eventType){
         case "BREACHED":
             // Create test ticket object
             let newTicket = new ticket(ticketID, ticketName, ticketURL, eventType);
             addTicket(newTicket);
-
+            break;
         default:
             removeTicket(ticketID);
     }
-    
-      
 }
 
-function addTicket(newTicket){
-    console.log('New ticket created.')
-    
+// Add ticket to database
+async function addTicket(newTicket) {
+    await kv.set(`ticket:${newTicket.ticketID}`, {
+        ticketID,
+        ticketName,
+        ticketURL,
+        eventType,
+    });
+    console.log(`Ticket ${ticketID} added.`);
 }
 
-function removeTicket(ticketID){
-
+// Remove ticket from database
+async function removeTicket(ticketID){
+    await currentBreached.delete([ticketID]);
 }
 
-handleRequest("442786", "Quiero recuperar unos archivos que no se guradaron bien", "https://goodnotes.zendesk.com/agent/tickets/442786", "BREACHED");
+async function sendSummary(){
+    const allTickets = await currentBreached.list({prefix: [""]});
+
+    const ticketsArray = [];
+
+    // Use for await to iterate over the iterable
+    for await (const ticket of allTickets) {
+        ticketsArray.push(ticket);
+    }
+    const date = Date.now();
+    let fullMessage = `SLA Summary for ${date}:\n
+                        # of breached tickets: ${allTickets.length}`;
+}
+
+handleRequest("442786", "Quiero recuperar unos archivos que no se guradaron bien", "link.com", "BREACHED");
+handleRequest("442787", "Test 2", "https://goodnotes.zendesk.com/agent/tickets/442787", "BREACHED");
+handleRequest("442788", "Test 3", "https://goodnotes.zendesk.com/agent/tickets/442788", "BREACHED");
+handleRequest("442789", "Test 4", "https://goodnotes.zendesk.com/agent/tickets/442789", "BREACHED");
+
+sendSummary();
